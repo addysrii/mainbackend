2ae8169b4d2d81cd49ec32cc7574711bc0a2eda6
UdayPandey01 <pandeyuday113@gmail.com>
2025-06-28 02:33:59 +0530

    fixed the image upload error

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,11 +6,6 @@ const { ProfileView } = require("../models/User");
 const { Achievement, Project } = require("../models/Portfolio");
 const Settings = require("../models/Settings");
 const Skill = require("../models/Skill");
-const fs = require("fs");
-const uploadDir = "uploads/profile";
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 /**
  * Get the current authenticated user
