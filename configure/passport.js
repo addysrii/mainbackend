ec0333c05157449f6a9eff8bb23002b7a5d788ab
addysrii <149177246+addysrii@users.noreply.github.com>
2025-07-22 17:50:00 +0530

    Update passport.js

--- configure/passport.js
+++ configure/passport.js
@@ -12,7 +12,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const LINKEDIN_CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
 const LINKEDIN_CLIENT_SECRET = process.env.LINKEDIN_CLIENT_SECRET;
-const BASE_URL = process.env.BASE_URL || 'https://new-backend-w86d.onrender.com';
+const BASE_URL = 'https://new-backend-w86d.onrender.com';
 console.log(BASE_URL)
 console.log(GOOGLE_CLIENT_SECRET)
 console.log(GOOGLE_CLIENT_ID)
